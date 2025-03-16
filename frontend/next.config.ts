6ee4d41db1a7d8b6290bcb0b9ce815a6b0ed7aee
pwh9882 <pwh9882@gmail.com>
2025-03-16 20:27:16 +0900

    💡 output: 'export' 설정을 통해 정적 사이트 (Static Export) 생성 가능 💡 next build && next export 실행 시 /out 디렉토리 생성 → S3에 업로드

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // 정적 사이트 생성 (S3 배포 가능)
+  trailingSlash: true, // S3 호환을 위해 URL 끝에 슬래시 추가
+  reactStrictMode: true,
 };
 
 export default nextConfig;
